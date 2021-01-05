a62912fe143c02bdac8cc976c54c0f2b60df2856
xcrone <zahircrone@gmail.com>
2021-01-05 14:45:45 +0800

    add comment

--- ReadWrite.sol
+++ ReadWrite.sol
@@ -2,9 +2,13 @@
 pragma solidity ^0.8.0;
 
 contract readWrite {
+    // public is for enabled read function
+    // use private to disabled read function
     string public name = "xcrone";
     uint256 public age = 21;
     
+    // public is for enabled write function
+    // use private to disabled write function
     function setName(string memory _name) public returns(string memory) {
         name = _name;
         return _name;
