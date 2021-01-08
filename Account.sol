e32d93b1ee10e2659a6cf28b73c13966335551ce
xcrone <zahircrone@gmail.com>
2021-01-09 04:05:13 +0800

    Update Account.sol

--- Account.sol
+++ Account.sol
@@ -3,5 +3,25 @@
 pragma solidity ^0.8.0;
 
 contract Account {
+    uint public value;
+
+    function msgSender() public view returns(address) {
+        // msg.sender show account that generate the transaction/function
+        return msg.sender;
+    }
+    
+    // send wei
+    function msgValue() payable public {
+        // msg.value is wei value that sent from account
+        value = msg.value;
+    }
+    
+    function msgGas() public view returns(uint) {
+        // msg.gas show the remaining gas of transaction
+        // msg.gas will be replaced with gasleft()
+        return gasleft();
+    }
+    
+    // TODO: msg.data => need more research
     
 }
